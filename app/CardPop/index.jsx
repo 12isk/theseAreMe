037dd8430857e001d361cc8cc258ca5add36cec7
12.isk <66267884+12isk@users.noreply.github.com>
2025-02-16 14:11:41 +0100

    slight delay in animation fixed

--- app/CardPop/index.jsx
+++ app/CardPop/index.jsx
@@ -90,14 +90,14 @@ export default function Index() {
             duration: 0.82,
             ease: "power3",
             yPercent: -50,
-            delay: 0.1,
+            //delay: 0.1,
         });
 
         const moveImageY = gsap.quickTo(imageRef.current, "top", {
             duration: 0.82,
             ease: "power3",
             xPercent: -50,
-            delay: 0.1,
+            //delay: 0.1,
         });
 
         const updatePosition = () => {
@@ -187,12 +187,12 @@ export default function Index() {
                     variants={imageVariants}
                     initial="closed"
                     animate={isHovering ? "visible" : "hidden"}
-                    mode="wait"
-                    transition={{
-                        type: "tween",
-                        duration: 0.5,
-                        ease: [0.76, 0, 0.24, 1]
-                    }}
+                    // mode="wait"
+                    // transition={{
+                    //     type: "tween",
+                    //     duration: 0.5,
+                    //     ease: [0.76, 0, 0.24, 1]
+                    // }}
                 >
                     <img
                         src={currentImage}
